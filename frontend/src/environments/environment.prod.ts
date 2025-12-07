d57e2755d231b476542e3f2c4fc1b6171405897b
Emerson Steven Bueno Salvin <135454197+STVNBN@users.noreply.github.com>
2025-12-07 17:07:09 -0600

    Clean up environment.prod.ts by removing comment
    Removed comment about backend deployment.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -6,7 +6,5 @@ export const environment = {
   NG_APP_AUTH0_DOMAIN: 'dev-yptklnlb6mdu01fy.us.auth0.com',
   NG_APP_AUTH0_CLIENT_ID: '42wpoqFaZYukXLhszzHoIGnpAKrbrXHk',
   NG_APP_AUTH0_AUDIENCE: 'https://api.mapa-riesgos',
-
-  // 👇 Backend desplegado en Render
   NG_APP_API_URL: 'https://mapa-riesgo-code-1.onrender.com'
 };
