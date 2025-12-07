47f4bf97f6777d13026a888ceecfdd0bd54808ea
Emerson Steven Bueno Salvin <135454197+STVNBN@users.noreply.github.com>
2025-12-07 17:06:18 -0600

    Update reportes.service.ts

--- frontend/src/app/reportes/reportes.service.ts
+++ frontend/src/app/reportes/reportes.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ReportesService {
-  private baseUrl = 'http://localhost:8080/api/reportes';
+  private baseUrl = `${environment.NG_APP_API_URL}/api/reportes`;
 
   constructor(private http: HttpClient) {}
 
