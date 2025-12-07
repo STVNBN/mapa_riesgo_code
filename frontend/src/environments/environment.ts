c26b01e5ca9e643628041f9942d6ca69153dd401
Emerson Steven Bueno Salvin <135454197+STVNBN@users.noreply.github.com>
2025-12-07 16:20:32 -0600

    Change API URL to localhost for development

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   NG_APP_AUTH0_DOMAIN: 'dev-yptklnlb6mdu01fy.us.auth0.com',
   NG_APP_AUTH0_CLIENT_ID: '42wpoqFaZYukXLhszzHoIGnpAKrbrXHk',
   NG_APP_AUTH0_AUDIENCE: 'https://api.mapa-riesgos',
-  NG_APP_API_URL: 'https://mapa-riesgo-code.onrender.com'
+  NG_APP_API_URL: 'http://localhost:8080'
 };
